52f0264a45c44066a70610804edb67244cf700a6
Whitney Williams <whitney_e_williams@homedepot.com>
2019-06-20 11:01:25 -0700

    remove unnecessary proptype object

--- src/examples/search/components/searchTableCells/sourceList.js
+++ src/examples/search/components/searchTableCells/sourceList.js
@@ -4,13 +4,10 @@ import _ from 'lodash';
 const SourceListComponent = (sources) => {
     const sourceListItems = _.map(sources, (source, i) => (<li key={i}>{source.name}</li>));
     return (
-      <ul className="plp-source-list">
-        {sourceListItems}
-      </ul>
+        <ul className="plp-source-list">
+            {sourceListItems}
+        </ul>
     );
-};
-
-SourceListComponent.propTypes = {
 };
 
 export default SourceListComponent;
